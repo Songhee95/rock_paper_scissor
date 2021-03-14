d82c721aae243bbc103e18f2333541908e8ec4b4
songhee <thdgml5537@gmail.com>
2021-03-14 18:47:06 -0400

    add switch case for computer random pick

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
 namespace rock_paper_scissor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputPlayer, inputCPU;
+            int randomInt;
+
+            Console.WriteLine("Choose between ROCK, PAPER, SCISSOR:  ");
+            inputPlayer = Console.ReadLine();
+
+            Random random = new Random();
+            randomInt = random.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "ROCK";
+                    Console.WriteLine("Computer : Rock");
+                    break;
+                case 2:
+                    inputCPU = "PAPER";
+                    Console.WriteLine("Computer : Paper");
+                    break;
+                case 3:
+                    inputCPU = "SCISSOR";
+                    Console.WriteLine("Computer: Scissor");
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry!");
+                    break;
+
+            }
         }
     }
 }
