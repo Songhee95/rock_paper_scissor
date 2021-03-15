54f13d99b793edec391a8b59b783935b2090ea0c
songhee <thdgml5537@gmail.com>
2021-03-14 20:28:32 -0400

    loop over for multiple game set

--- Program.cs
+++ Program.cs
@@ -9,76 +9,95 @@ namespace rock_paper_scissor
             int randomInt;
             int playerScore = 0;
             int CpuScore = 0;
+            bool playAgain = true;
             string template = "Player: {0} - CPU: {1}";
 
-            Console.WriteLine("Choose between ROCK, PAPER, SCISSOR:  ");
-            inputPlayer = Console.ReadLine();
-            inputPlayer = inputPlayer.ToUpper();
-
-            Random random = new Random();
-            randomInt = random.Next(1, 4);
-
-            switch (randomInt)
+            while (playAgain)
             {
-                case 1:
-                    inputCPU = "ROCK";
-                    Console.WriteLine("Computer : Rock");
-                    if (inputPlayer == "ROCK")
+                while (playerScore < 3 && CpuScore < 3)
+                {
+                    Console.WriteLine("Choose between ROCK, PAPER, SCISSOR:  ");
+                    inputPlayer = Console.ReadLine();
+                    inputPlayer = inputPlayer.ToUpper();
+
+                    Random random = new Random();
+                    randomInt = random.Next(1, 4);
+
+                    switch (randomInt)
                     {
-                        Console.WriteLine("Draw!!\n");
+                        case 1:
+                            inputCPU = "ROCK";
+                            Console.WriteLine("Computer : Rock");
+                            if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("Draw!!\n");
+                            }
+                            else if (inputPlayer == "PAPER")
+                            {
+                                Console.WriteLine("You Won!!\n");
+                                playerScore++;
+                            }
+                            else if (inputPlayer == "SCISSOR")
+                            {
+                                Console.WriteLine("You Lost!!\n");
+                                CpuScore++;
+                            }
+                            break;
+                        case 2:
+                            inputCPU = "PAPER";
+                            Console.WriteLine("Computer : Paper");
+                            if (inputPlayer == "PAPER")
+                            {
+                                Console.WriteLine("Draw!!\n");
+                            }
+                            else if (inputPlayer == "SCISSOR")
+                            {
+                                Console.WriteLine("You Won!!\n");
+                                playerScore++;
+                            }
+                            else if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("You Lost!!\n");
+                                CpuScore++;
+                            }
+                            break;
+                        case 3:
+                            inputCPU = "SCISSOR";
+                            Console.WriteLine("Computer: Scissor");
+                            if (inputPlayer == "SCISSOR")
+                            {
+                                Console.WriteLine("Draw!!\n");
+                            }
+                            else if (inputPlayer == "ROCK")
+                            {
+                                Console.WriteLine("You Won!!\n");
+                                playerScore++;
+                            }
+                            else if (inputPlayer == "PAPER")
+                            {
+                                Console.WriteLine("You Lost!!\n");
+                                CpuScore++;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Invalid entry!");
+                            break;
                     }
-                    else if (inputPlayer == "PAPER")
-                    {
-                        Console.WriteLine("You Won!!\n");
-                        playerScore++;
-                    }
-                    else if (inputPlayer == "SCISSOR")
-                    {
-                        Console.WriteLine("You Lost!!\n");
-                        CpuScore++;
-                    }
-                    break;
-                case 2:
-                    inputCPU = "PAPER";
-                    Console.WriteLine("Computer : Paper");
-                    if (inputPlayer == "PAPER")
-                    {
-                        Console.WriteLine("Draw!!\n");
-                    }
-                    else if (inputPlayer == "SCISSOR")
-                    {
-                        Console.WriteLine("You Won!!\n");
-                        playerScore++;
-                    }
-                    else if (inputPlayer == "ROCK")
-                    {
-                        Console.WriteLine("You Lost!!\n");
-                        CpuScore++;
-                    }
-                    break;
-                case 3:
-                    inputCPU = "SCISSOR";
-                    Console.WriteLine("Computer: Scissor");
-                    if (inputPlayer == "SCISSOR")
-                    {
-                        Console.WriteLine("Draw!!\n");
-                    }
-                    else if (inputPlayer == "ROCK")
-                    {
-                        Console.WriteLine("You Won!!\n");
-                        playerScore++;
-                    }
-                    else if (inputPlayer == "PAPER")
-                    {
-                        Console.WriteLine("You Lost!!\n");
-                        CpuScore++;
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Invalid entry!");
-                    break;
+
+                }
+                Console.WriteLine(template, playerScore, CpuScore);
+                Console.WriteLine("Do you want to play Again?");
+                string loop = Console.ReadLine();
+                if (loop == "no")
+                {
+                    playAgain = false;
+                }
+                else
+                {
+                    playerScore = 0;
+                    CpuScore = 0;
+                }
             }
-            // Console.WriteLine(template, playerScore, CpuScore);
         }
     }
 }
